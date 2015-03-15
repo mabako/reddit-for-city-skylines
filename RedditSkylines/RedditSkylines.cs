e71a586d41a7c4da69f920a3753066d99da06f92
Marcus Bauer <mabako@gmail.com>
2015-03-16 00:12:17 +0100

    Initial Commit

---
+++ RedditSkylines/RedditSkylines.cs
@@ -0,0 +1,73 @@
+﻿using ColossalFramework;
+using ICities;
+using System;
+using System.Text;
+using System.Timers;
+
+namespace RedditClient
+{
+    public class ModInfo : IUserMod
+    {
+        public string Description
+        {
+            get { return "Show what's new on /r/showerthoughts"; }
+        }
+
+        public string Name
+        {
+            get { return "Reddit for Chirpy"; }
+        }
+    }
+
+    public class RedditUpdater : ChirperExtensionBase
+    {
+        private Timer timer = new Timer();
+        private string lastPostId = null;
+
+        public override void OnCreated(IChirper threading)
+        {
+            DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Loaded RedditX thing");
+            timer.AutoReset = true;
+            timer.Elapsed += new ElapsedEventHandler((sender, e) => UpdateRedditPosts());
+            timer.Interval = 5 * 60 * 1000;
+            timer.Start();
+        }
+
+        public override void OnReleased()
+        {
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        private void UpdateRedditPosts()
+        {
+            try
+            {
+                RedditPost newestPost = TinyWeb.FindLastPost("showerthoughts");
+                if (newestPost.id != lastPostId)
+                {
+                    AddMessage(new Message { senderName = string.Format("{0}", newestPost.author), text = newestPost.title });
+                    lastPostId = newestPost.id;
+                }
+            }
+            catch (Exception e)
+            {
+                DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Failed to update reddit posts: " + e.GetType().ToString() + ": " + e.Message); 
+            }
+        }
+
+        private void AddMessage(Message m)
+        {
+            Singleton<ChirpPanel>.instance.AddMessage(m);
+        }
+    }
+    public class Message : IChirperMessage
+    {
+        public uint senderID
+        {
+            get { return 0; }
+        }
+        public string senderName { get; set; }
+        public string text { get; set; }
+    }
+}
---
+++ RedditSkylines/TinyWeb.cs
@@ -0,0 +1,45 @@
+﻿using SimpleJson;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditClient
+{
+    internal class TinyWeb
+    {
+        private const string SHOWERTHOUGHTS = "http://www.reddit.com/r/{0}/new.json?limit=1";
+        public static RedditPost FindLastPost(string subreddit)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(string.Format(SHOWERTHOUGHTS, subreddit));
+            request.Method = WebRequestMethods.Http.Get;
+            request.Accept = "text/json";
+
+            using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                    return null;
+
+                string str = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                
+                JsonObject root = (JsonObject)SimpleJson.SimpleJson.DeserializeObject(str);
+                JsonObject rootData = (JsonObject)root["data"];
+                JsonArray rootChildren = (JsonArray)rootData["children"];
+                JsonObject firstChild = (JsonObject)rootChildren[0];
+                JsonObject first = (JsonObject)firstChild["data"];
+
+                return new RedditPost { id = first["id"].ToString(), title = first["title"].ToString(), author = "/u/" + first["author"].ToString() };
+            }
+        }
+    }
+
+    internal class RedditPost
+    {
+        internal string title;
+        internal string author;
+        internal string id;
+    }
+}
